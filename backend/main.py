54777957a8787eddffca837e3720a5f449395c99
Aditya Kumar Sinha <adityauv11@gmail.com>
2025-08-25 14:45:03 +0530

    main file

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 import modal
+import os
 
-app = modal.app("music-generator")
+app = modal.App("music-generator")
 
 image = (
     modal.Image.debian_slim()
@@ -15,3 +16,55 @@ model_volume = modal.Volume.from_name(
     "ace-step-models", create_if_missing=True
 )
 hf_volume = modal.Volume.from_name("gwen-hf-cache", create_if_missing=True)
+
+music_gen_secrets = modal.Secret.from_name("music-gen-secret")
+
+@app.cls(
+    image=image,
+    gpu="L40S",
+    volumes={"/models": model_volume, "/.cache/huggingface": hf_volume},
+    secrets=[music_gen_secrets],
+    scaledown_window=15
+)
+
+class MusicGenServer:
+    @modal.enter()
+    def load_model(self):
+        from acestep.pipeline_ace_step import ACEStepPipeline
+        from transformers import AutoModelForCausalLM, AutoTokenizer
+        from diffusers import AutoPipelineForText2Image
+        import torch
+
+        # music generation model
+        self.music_model = ACEStepPipeline(
+            checkpoint_dir="/models",
+            dtype="bfloat16",
+            torch_compile=False,
+            cpu_offload=False,
+            overlapped_decode=False
+        )
+
+        # Large Language Model
+        model_id = "Qwen/Qwen2-7B-Instruct"
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+        self.llm_model = AutoModelForCausalLM.from_pretrained(
+            model_id,
+            torch_dtype="auto",
+            device_map="auto",
+            cache_dir="/.cache/huggingface"
+        )
+
+        # Stable Diffusion Model (thumbnails)
+        self.image_pipe = AutoPipelineForText2Image.from_pretrained(
+            "stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16", cache_dir="/.cache/huggingface")
+        self.image_pipe.to("cuda")
+
+    @modal.fastapi_endpoint(method="POST")
+    def generate(self):
+        # Implement music generation logic here
+        pass
+
+@app.local_entrypoint()
+def main():
+    function_test.remote()
